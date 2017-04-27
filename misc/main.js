7fbc3c7bf8bc2f3eaa5aa57e80e42e94c0a253af
KieranSherman <15kpsherman@gmail.com>
2017-04-27 14:24:03 -0400

    Auto-update from CLI @ 14:24:03--04/27 [Access-code-redirect]

--- misc/main.js
+++ misc/main.js
@@ -1,6 +1,29 @@
-function autotab(current,to) {
+function autoTab(current,to) {
   to.select();
 
   if(current.value.length == current.getAttribute("maxlength"))
     to.focus();
+}
+
+function doOnClick(current) {
+  current.select();
+}
+
+function checkValid(form) {
+  var word = "LLAMA".split('');
+  var valid = true;
+
+  for(var i = 0; i < word.length; i++) {
+    var c = word[i];
+    var name = "input"+i;
+    var val = form.elements[i].value;
+
+    if(val.toUpperCase() != c.toUpperCase()) {
+      valid = false;
+      break;
+    }
+  }
+
+  if(valid)
+    window.location.assign("dev/index.html");
 }
